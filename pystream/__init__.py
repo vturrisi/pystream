c6532320564756c3a28b1bffa369e61a839addcb
Victor Turrisi <vt.turrisi@gmail.com>
2018-12-11 16:49:24 -0200

    added exception test to init file

--- pystream/__init__.py
+++ pystream/__init__.py
@@ -1,4 +1,7 @@
-from . import algorithms, evaluation, utils
-
+try:
+    from . import algorithms, evaluation, utils
+except ImportError:
+    msg = 'Error importing pystream: do not import from its source directory'
+    raise ImportError(msg)
 
 __all__ = ['algorithms', 'evaluation', 'utils']

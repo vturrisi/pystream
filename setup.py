a1b234733252e34b8f6b2bb30beab771df1f3f78
vturrisi <vt.turrisi@gmail.com>
2019-01-26 20:42:37 -0200

    reworked building system

--- setup.py
+++ setup.py
@@ -1,31 +1,155 @@
-from numpy.distutils.core import setup
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+import Cython
+import numpy as np
+import os
+
+libs = []
+if os.name == 'posix':
+    libs.append('m')
+
+dirs = [np.get_include(), '.']
+
+extensions = []
+
+# ----------------------------- algorithms -----------------------------
+
+# ----------------------------- base -----------------------------
+
+dependencies = ['pystream/algorithms/nominal_counters/nominal_counter.pxd'
+                'pystream/algorithms/nominal_counters/nominal_counter.pxd']
+
+E = Extension(name='pystream.algorithms.base.vfdt',
+              sources=['pystream/algorithms/base/vfdt.pyx'],
+              libraries=libs,
+              include_dirs=dirs,
+              depends=dependencies)
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.base.svfdt',
+              sources=['pystream/algorithms/base/svfdt.pyx'],
+              libraries=libs,
+              include_dirs=dirs,
+              depends=dependencies + ['pystream/algorithms/base/vfdt.pxd'])
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.base.svfdt_ii',
+              sources=['pystream/algorithms/base/svfdt_ii.pyx'],
+              libraries=libs,
+              include_dirs=dirs,
+              depends=dependencies + ['pystream/algorithms/base/vfdt.pxd',
+                                      'pystream/algorithms/base/svfdt.pxd'])
+extensions.append(E)
+
+# ----------------------------- change_detectors -----------------------------
+
+E = Extension(name='pystream.algorithms.change_detectors.adwin',
+              sources=['pystream/algorithms/change_detectors/adwin.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+# ----------------------------- ensembles -----------------------------
+
+dependencies = ['pystream/algorithms/change_detectors/adwin.pxd']
+
+E = Extension(name='pystream.algorithms.ensembles.arf',
+              sources=['pystream/algorithms/ensembles/arf.pyx'],
+              libraries=libs,
+              include_dirs=dirs,
+              depends=dependencies)
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.ensembles.leveraging_bagging',
+              sources=['pystream/algorithms/ensembles/leveraging_bagging.pyx'],
+              libraries=libs,
+              include_dirs=dirs,
+              depends=dependencies)
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.ensembles.oaue',
+              sources=['pystream/algorithms/ensembles/oaue.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.ensembles.ozabagging',
+              sources=['pystream/algorithms/ensembles/ozabagging.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.ensembles.ozaboosting',
+              sources=['pystream/algorithms/ensembles/ozaboosting.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+# ----------------------------- nominal_counters -----------------------------
+
+E = Extension(name='pystream.algorithms.base.nominal_counters.nominal_counter',
+              sources=['pystream/algorithms/base/nominal_counters/nominal_counter.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+# ----------------------------- numeric_estimators -----------------------------
+
+E = Extension(name='pystream.algorithms.base.numeric_estimators.gaussian_estimator',
+              sources=['pystream/algorithms/base/numeric_estimators/gaussian_estimator.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+# ----------------------------- statistics -----------------------------
+
+E = Extension(name='pystream.algorithms.base.statistics.tree_stats',
+              sources=['pystream/algorithms/base/statistics/tree_stats.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
+
+E = Extension(name='pystream.algorithms.base.statistics.value_stats',
+              sources=['pystream/algorithms/base/statistics/value_stats.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
 
 
-def configuration(parent_package='',top_path=None):
-    from Cython.Build import cythonize
-    from numpy.distutils.misc_util import Configuration
+# ----------------------------- evaluation -----------------------------
 
+E = Extension(name='pystream.evaluation.evaluate_prequential',
+              sources=['pystream/evaluation/evaluate_prequential.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
 
-    config = Configuration(None, parent_package, top_path)
+E = Extension(name='pystream.evaluation.performance_statistics',
+              sources=['pystream/evaluation/performance_statistics.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
 
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
+# ----------------------------- utils -----------------------------
 
-    config.add_subpackage('pystream')
-
-    config.ext_modules = cythonize(config.ext_modules, nthreads=4)
-    return config
-
+E = Extension(name='pystream.utils.stream_gen',
+              sources=['pystream/utils/stream_gen.pyx'],
+              libraries=libs,
+              include_dirs=dirs)
+extensions.append(E)
 
 setup(name='pystream',
-      version='1.1',
+      version='1.0',
       description='Pystream',
       url='http://github.com/vturrisi/pystream',
       author='Victor Turrisi',
       license='MIT',
-      **configuration().todict(),
+      packages=find_packages(exclude=['tests']),
+      quiet=True,
+      ignore_setup_xxx_py=True,
+      assume_default_configuration=True,
+      ext_modules=cythonize(extensions, nthreads=4, quiet=False),
+      cmdclass={'build_ext': Cython.Build.build_ext},
       setup_requires=['cython>=0.x'],
       install_requires=['numpy>=1.14.1', 'pandas>=0.20.0', 'cython>=0.x'],
       include_package_data=True,
